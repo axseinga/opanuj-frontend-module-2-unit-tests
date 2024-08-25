c023ea1c002775f0707fb7fce578681b227095d0
axseinga <urbanowicz.uk@gmail.com>
2024-08-25 20:20:35 +0100

    fix: imports

--- src/pages/book-shop/book-shop.test.tsx
+++ src/pages/book-shop/book-shop.test.tsx
@@ -4,8 +4,8 @@ import "@testing-library/jest-dom/vitest";
 import { test, expect, afterEach } from "vitest";
 import { cleanup, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { BookShop } from "../pages/book-shop/book-shop";
-import { data } from "../data";
+import { BookShop } from "../../pages/book-shop/book-shop";
+import { data } from "../../data";
 
 afterEach(cleanup);
 
