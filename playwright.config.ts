d81d712be4ba9086910f85715b5688ed068a582d
axseinga <urbanowicz.uk@gmail.com>
2024-09-03 17:45:09 +0200

    add e2e test

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: `src/tests/e2e`,
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['html', { outputFolder: `src/tests/playwright/html-report` }],
+  ],
+  use: {
+    baseURL: process.env.CI ? process.env.E2E_TESTS_BASE_URL : 'http://localhost:5173',
+    trace: 'on',
+  },
+  outputDir: `src/tests/playwright/test-results`,
+});
---
+++ src/tests/utils/consts.ts
@@ -0,0 +1,4 @@
+export const PAGES = {
+  HOMEPAGE: "/",
+  PRODUCT_DETAILS_PAGE: (id: string) => `/#/info/${id}`,
+};
